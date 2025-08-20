281a9b6787cb630714f4200389990c2ebbf26f64
Sachin Shet <sachinshet135@gmail.com>
2025-08-20 18:41:20 -0400

    send message and check in confluent

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -48,7 +48,9 @@ def stream_data():
     res = format_data(res)
     print(json.dumps(res,indent=3))
     
-    producer = KafkaProducer(bootstrap_servers=['broker'])
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+
+    producer.send('users_created', json.dumps(res).encode('utf-8'))
 
 """ with DAG('user_automation',
          default_args=default_args,
